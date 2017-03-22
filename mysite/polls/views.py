a3dbbd44d3b012d1a357241b4008d6e99074a39c
Roman TSovanyan <covrom@gmail.com>
2017-03-22 13:04:08 +0300

    first views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('Всем привет! Мой сайт работает!')
+    #pylint: disable=no-member
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse('Вы смотрите вопрос %s.' % question_id)
+
+def results(request, question_id):
+    response = 'Вы смотрите результаты вопроса %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Вы голосуете по вопросу %s.' % question_id)
