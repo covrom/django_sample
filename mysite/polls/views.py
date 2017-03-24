a5874b9ac69f439f708c3bdbc19e919765e62a7d
Roman TSovanyan <covrom@gmail.com>
2017-03-24 13:53:28 +0300

    import change

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
